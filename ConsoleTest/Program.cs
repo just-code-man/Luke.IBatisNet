c0605869d6da49f71cf5c6960c34378b92adab0d
LK <1455832457@qq.com>
2024-07-20 04:16:45 +0800

    提交迁移代码

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using Luke.IBatisNet.DataMapper;
+using Luke.IBatisNet.DataMapper.Configuration;
+using System.Data;
+using System.Threading.Channels;
+
+namespace ConsoleTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DomSqlMapBuilder builder = new DomSqlMapBuilder();
+            ISqlMapper mapper = builder.Configure("SqlMap.config");
+
+            DataTable dt = mapper.QueryForDataTable("TestSql", "xxxx");
+
+            foreach (DataRow row in dt.Rows)
+            {
+                foreach (DataColumn col in dt.Columns)
+                {
+                    Console.WriteLine(row[col.ColumnName].ToString());
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Luke.IBatisNet.DataAccess/SessionStore/ThreadSessionStore.cs
@@ -0,0 +1,56 @@
+﻿using Luke.IBatisNet.Common;
+using Luke.IBatisNet.DataMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Luke.IBatisNet.DataAccess.SessionStore
+{
+    public class ThreadSessionStore : AbstractSessionStore
+    {
+        [ThreadStatic]
+        private static readonly Dictionary<string, IDalSession> StaticSessions =
+            new Dictionary<string, IDalSession>();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CallContextSessionStore"/> class.
+        /// </summary>
+        /// <param name="sqlMapperId">The SQL mapper id.</param>
+        public ThreadSessionStore(string sqlMapperId)
+            : base(sqlMapperId)
+        {
+        }
+
+        /// <summary>
+        /// Get the local session
+        /// </summary>
+        public override IDalSession LocalSession
+        {
+            get
+            {
+                IDalSession result;
+                StaticSessions.TryGetValue(sessionName, out result);
+                return result;
+                //             return StaticSessions[sessionName] as SqlMapSession;
+            }
+        }
+
+        /// <summary>
+        /// Store the specified session.
+        /// </summary>
+        /// <param name="session">The session to store</param>
+        public override void Store(IDalSession session)
+        {
+            StaticSessions[sessionName] = session;
+        }
+
+        /// <summary>
+        /// Remove the local session.
+        /// </summary>
+        public override void Dispose()
+        {
+            StaticSessions[sessionName] = null;
+        }
+
+    }
+}
